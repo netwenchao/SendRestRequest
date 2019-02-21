a23df4ebbc46fe5839baeac3f148afe1ac940625
Denny <netwenchao@hotmail.com>
2019-02-21 23:36:16 +0800

    Complete HttpUtils

--- CommonLib/HttpUtils.cs
+++ CommonLib/HttpUtils.cs
@@ -1,12 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace CommonLib
 {
+    public interface ILog
+    {
+        void Info(string msg);
+        void Error(string errMsg);
+        void Error(Exception exp);
+        void Warning(string msg);
+    }
+
     public class HttpUtils
     {
+        #region core
         /// <summary>
         /// 发送HttpRequest
         /// </summary>
@@ -15,12 +30,14 @@ namespace CommonLib
         /// <param name="url"></param>
         /// <param name="headers"></param>
         /// <returns></returns>
-        public static async Task<string> SendRequestWithStringOut(HttpMethod method,string content,string url,Dictionary<string,string> headers=null)
+        public static async Task<string> SendRequest(HttpMethod method, string content, string url, Dictionary<string, string> headers = null)
         {
-            var client=new HttpClient();
-            var request=BuildRequest(method, url, content, headers);
-            var response=await client.SendAsync(request);
-            return await response.Content.ReadAsStringAsync();
+            using (var client = new HttpClient())
+            {
+                var request = BuildRequest(method, url, content, headers);
+                var response = await client.SendAsync(request);
+                return await response.Content.ReadAsStringAsync();
+            }
         }
 
         /// <summary>
@@ -31,12 +48,14 @@ namespace CommonLib
         /// <param name="url"></param>
         /// <param name="headers"></param>
         /// <returns></returns>
-        public static async Task<Stream> SendRequestWithStreamOut(HttpMethod method, string content, string url, Dictionary<string, string> headers = null)
+        public static async Task<Stream> SendRequestStream(HttpMethod method, string content, string url, Dictionary<string, string> headers = null)
         {
-            var client = new HttpClient();
-            var request = BuildRequest(method, url, content, headers);
-            var response = await client.SendAsync(request);
-            return await response.Content.ReadAsStreamAsync();
+            using (var client = new HttpClient())
+            {
+                var request = BuildRequest(method, url, content, headers);
+                var response = await client.SendAsync(request);
+                return await response.Content.ReadAsStreamAsync();
+            }
         }
 
 
@@ -48,9 +67,9 @@ namespace CommonLib
         /// <param name="content"></param>
         /// <param name="headers"></param>
         /// <returns></returns>
-        public static HttpRequestMessage BuildRequest(HttpMethod method, string url,string content, Dictionary<string, string> headers=null)
+        public static HttpRequestMessage BuildRequest(HttpMethod method, string url, string content, Dictionary<string, string> headers = null, string contentType = "application/json")
         {
-            var request = new HttpRequestMessage(method, url);
+            var request = new HttpRequestMessage() { Method = method };
             if (headers != null)
             {
                 foreach (var header in headers)
@@ -58,8 +77,19 @@ namespace CommonLib
                     request.Headers.Add(header.Key, header.Value);
                 }
             }
+            request.RequestUri = new Uri(url);
+            if (string.IsNullOrWhiteSpace(contentType))
+            {
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+            }
+            if (!string.IsNullOrEmpty(content))
+            {
+                request.Content = new StringContent(content);
+            }
             return request;
         }
+
+        #endregion
 
         /// <summary>
         /// Post 并获取Json类型返回值
@@ -69,11 +99,11 @@ namespace CommonLib
         /// <param name="url"></param>
         /// <param name="request"></param>
         /// <returns></returns>
-        public static async Task<TResponse> PostAsJson<TRequest,TResponse>(string url, TRequest request)
+        public static async Task<TResponse> PostAsJson<TRequest, TResponse>(string url, TRequest request, Dictionary<string, string> header)
         {
-            var client=new HttpClient();
-            var response=await client.PostAsJsonAsync(url, request);
-            return await response.Content.ReadAsAsync<TResponse>();
+            var response = SendRequest(HttpMethod.Post, JsonConvert.SerializeObject(request), url, header);
+            var responseText = await response;
+            return JsonConvert.DeserializeObject<TResponse>(responseText);
         }
 
         /// <summary>
@@ -84,9 +114,9 @@ namespace CommonLib
         /// <returns></returns>
         public static async Task<TResponse> GetAsJson<TResponse>(string url)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(url);
-            return await response.Content.ReadAsAsync<TResponse>();
+            var response = SendRequest(HttpMethod.Get, null, url);
+            var responseText = await response;
+            return JsonConvert.DeserializeObject<TResponse>(responseText);
         }
 
         /// <summary>
@@ -97,10 +127,95 @@ namespace CommonLib
 
         public static async Task<string> GetAsString(string url)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(url);
-            return await response.Content.ReadAsStringAsync();
+            return await SendRequest(HttpMethod.Get, null, url);
         }
-        
+    }
+
+    public sealed class PercentEncoding
+    {
+        public static string GenerateSignature(string url, string method, string requestBody, string appSecret, StringBuilder log = null, bool incHost = true)
+        {
+            var combined = new List<string>();
+            // request method
+            combined.Add(method.ToUpper());
+            Uri uri = new Uri(url);
+            if (incHost)
+            {
+                // scheme
+                combined.Add(uri.Scheme.ToLower());
+                // host
+                combined.Add(uri.Host.ToLower());
+                // port
+                combined.Add(uri.Port.ToString());
+            }
+            // path
+            string path = uri.AbsolutePath.ToLower();
+            path = path.Replace("\\", "/");
+            if (path.EndsWith("/"))
+                path = path.Substring(0, path.Length - 1);
+            combined.Add(PercentEncoding.Encode(path));
+
+            // query string
+            string q = (uri.Query ?? "").Trim();
+            if (q.Length > 0)
+            {
+                if (q.StartsWith("?"))
+                    q = q.Substring(1);
+                string[] itemStrs = q.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+                List<KeyValuePair<string, string>> items = new List<KeyValuePair<string, string>>();
+                foreach (string itemStr in itemStrs)
+                {
+                    if (itemStr.Trim().Length == 0) continue;
+                    string key = "", value = "";
+
+                    int index = itemStr.IndexOf("=");
+                    if (index <= 0) // = is missing or key is missing, ignore
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        key = HttpUtility.UrlDecode(itemStr.Substring(0, index)).Trim().ToLower();
+                        value = HttpUtility.UrlDecode(itemStr.Substring(index + 1)).Trim();
+                        items.Add(new KeyValuePair<string, string>(key, value));
+                    }
+                }
+
+                // query
+                combined.Add(String.Join("&", items.OrderBy(t => t.Key).Select(t => String.Format("{0}={1}", PercentEncoding.Encode(t.Key), PercentEncoding.Encode(t.Value))).ToArray()));
+            }
+            else
+                combined.Add("");
+
+            // body
+            combined.Add(PercentEncoding.Encode(requestBody ?? ""));
+            // salt
+            combined.Add(appSecret);
+
+            string baseString = String.Join("|", combined.ToArray());
+            if (log != null)
+                log.AppendLine("Base String: " + baseString);
+
+            System.Security.Cryptography.SHA256Managed s256 = new System.Security.Cryptography.SHA256Managed();
+            byte[] buff;
+            buff = s256.ComputeHash(Encoding.UTF8.GetBytes(baseString));
+            s256.Clear();
+            return Convert.ToBase64String(buff);
+        }
+
+        public static string Encode(string s)
+        {
+            string t = HttpUtility.UrlEncode(s);
+            t = t.Replace("+", "%20");
+            t = t.Replace("!", "%21");
+            t = t.Replace("(", "%28");
+            t = t.Replace(")", "%29");
+            t = t.Replace("*", "%2a");
+            return t;
+        }
+        public static string Decode(string s)
+        {
+            return HttpUtility.UrlDecode(s);
+        }
     }
 }
